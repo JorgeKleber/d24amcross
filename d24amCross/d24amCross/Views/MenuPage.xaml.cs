0ba42656cb54e68007cd12f13669aa668d53aaa0
Jorge Kleber Rabelo dos Santos <jorgekleber.santos2012@hotmail.com>
2017-03-20 17:51:15 -0400

    Revisão nas questões dos icones e imagens do projeto UWP

--- d24amCross/d24amCross/Views/MenuPage.xaml.cs
+++ d24amCross/d24amCross/Views/MenuPage.xaml.cs
@@ -26,9 +26,7 @@ namespace d24amCross.Views
 
             List<MenuModel> menuItems;
 
-            if ( Device.OS == TargetPlatform.Android || Device.OS == TargetPlatform.iOS )
-            {
-                menuItems = new List<MenuModel>
+            menuItems = new List<MenuModel>
                 {
                     new MenuModel { Opcao = "Notícias",   IndexItem = MenuIndex.Home,     IconeSrc = "noticia.png"    },
                     new MenuModel { Opcao = "Esportes",   IndexItem = MenuIndex.Eportes,  IconeSrc = "sports.png",},
@@ -37,21 +35,7 @@ namespace d24amCross.Views
                     new MenuModel { Opcao = "Sobre",      IndexItem = MenuIndex.Sobre,    IconeSrc = "sobre.png",    },
                 };
 
-                //this.ImagemLogo.Source = ImageSource.FromFile( "d24amLogo.jpg" );
-            }
-            else
-            {
-                menuItems = new List<MenuModel>
-                {
-                    new MenuModel { Opcao = "Notícias",   IndexItem = MenuIndex.Home,     IconeSrc = "ms-appx:///Imagens/noticia.png" },
-                    new MenuModel { Opcao = "Esportes",   IndexItem = MenuIndex.Eportes,  IconeSrc = "ms-appx:///Imagens/sports.png", },
-                    new MenuModel { Opcao = "Plus",       IndexItem = MenuIndex.Plus,     IconeSrc = "ms-appx:///Imagens/plus.png",   },
-                    new MenuModel { Opcao = "Amazônia",   IndexItem = MenuIndex.Amazonia, IconeSrc = "ms-appx:///Imagens/folha.png",  },
-                    new MenuModel { Opcao = "Sobre",      IndexItem = MenuIndex.Sobre,    IconeSrc = "ms-appx:///Imagens/sobre.png",  },
-                };
-
-                //this.ImagemLogo.Source = ImageSource.FromFile( "ms-appx:///d24amLogo.jpg" );
-            }
+            //this.ImagemLogo.Source = ImageSource.FromFile( "d24amLogo.jpg" );
 
             ListViewMenu.ItemsSource = menuItems;
 
